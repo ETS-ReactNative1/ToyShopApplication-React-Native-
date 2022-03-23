51a2c6c3d28985f8d6138029a3a1433dac2864e0
pratik <pratikpatre2019@gmail.com>
2022-03-23 15:29:11 +0530

    HomeScreen WILP

--- Components/CustomHeader.js
+++ Components/CustomHeader.js
@@ -3,26 +3,29 @@ import React from 'react';
 import {fonststyle} from '../Config/fontstyles';
 import BackIcon from 'react-native-vector-icons';
 
-export default function CustomHeader({goback, screenname}) {
-  console.log({goback});
+export default function CustomHeader({arrow, screenname}) {
+  console.log(arrow, 'Coming from custo header');
+
+  //The problem isin the Vector icon
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.logocontainer} onPress={goback}>
-        {/* TODO: 
-        1 > Name and Back Button 
-        2 > if name prop is enable then show LOgo only ontherwise display buttom 
-        3> OR Only in the HomeScreen Show Logo
-      */}
-
-        {goback == 'true' ? (
-          <BackIcon name="arrowleft" />
-        ) : (
+      {(arrow && (
+        <TouchableOpacity style={styles.logocontainer}>
+          <View>
+            <Text style={styles.logo}>Animos arrow</Text>
+            <Text style={styles.logosub}>Toy Store</Text>
+          </View>
+        </TouchableOpacity>
+      )) || (
+        <TouchableOpacity style={styles.logocontainer}>
           <View>
             <Text style={styles.logo}>Animos</Text>
             <Text style={styles.logosub}>Toy Store</Text>
           </View>
-        )}
-      </TouchableOpacity>
+        </TouchableOpacity>
+      )}
+
       {screenname ? (
         <View style={{backgroundColor: 'red'}}>
           <Text style={styles.screenname}>{screenname}</Text>
--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+
 import CustomHeader from '../Components/CustomHeader';
 import Screeen from '../Components/Screeen';
 
 export default function ProfileScreen() {
+  [arrow, Setarrow] = useState(null);
+
+  useEffect(() => {
+    Setarrow(true);
+  }, []);
+
   return (
     <Screeen>
       <View style={styles.container}>
-        <CustomHeader screenname="Profile" goback="true" />
+        <CustomHeader screenname="Profile" arrow={arrow} />
       </View>
     </Screeen>
   );
